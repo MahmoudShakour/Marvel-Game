eba22215de557acde16e96191e94c96ff1233981
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-09-11 08:37:05 +0300

    Complete Champion class - basic implementation

---
+++ Marvel-Game/src/main/java/org/example/world/Champion.java
@@ -0,0 +1,124 @@
+package world;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+import model.abilities.Ability;
+import model.effects.Effect;
+
+public abstract class Champion {
+    private String name;
+    private int maxHP;
+    private int currentHP;
+    private int mana;
+    private int maxActionPointsPerTurn;
+    private int currentActionPoints;
+    private int attackRange;
+    private int attackDamage;
+    private int speed;
+    private ArrayList<Effect> appliedEffects;
+    private ArrayList<Ability> abilities;
+    private Condition condition;
+    private Point location;
+
+    public Champion(String name, int maxHP, int mana, int maxActions, int speed, int attackRange, int attackDamage) {
+        this.name = name;
+        this.maxHP = maxHP;
+        this.currentHP = maxHP;
+        this.mana = mana;
+        this.maxActionPointsPerTurn = maxActions;
+        this.currentActionPoints = maxActions;
+        this.attackRange = attackRange;
+        this.attackDamage = attackDamage;
+        this.speed = speed;
+        this.appliedEffects = new ArrayList<>();
+        this.abilities = new ArrayList<>();
+        this.condition = Condition.ACTIVE;
+        this.location = new Point();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxHP() {
+        return maxHP;
+    }
+
+    public int getCurrentHP() {
+        return currentHP;
+    }
+
+    public void setCurrentHP(int currentHP) {
+        this.currentHP = currentHP;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public int getMaxActionPointsPerTurn() {
+        return maxActionPointsPerTurn;
+    }
+
+    public void setMaxActionPointsPerTurn(int maxActionPointsPerTurn) {
+        this.maxActionPointsPerTurn = maxActionPointsPerTurn;
+    }
+
+    public int getCurrentActionPoints() {
+        return currentActionPoints;
+    }
+
+    public void setCurrentActionPoints(int currentActionPoints) {
+        this.currentActionPoints = currentActionPoints;
+    }
+
+    public int getAttackRange() {
+        return attackRange;
+    }
+
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public void setAttackDamage(int attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public ArrayList<Effect> getAppliedEffects() {
+        return appliedEffects;
+    }
+
+    public ArrayList<Ability> getAbilities() {
+        return abilities;
+    }
+
+    public Condition getCondition() {
+        return condition;
+    }
+
+    public void setCondition(Condition condition) {
+        this.condition = condition;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+}
