276d0f2bed505af88a27c2e631c17deedaf789b4
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-09-12 13:35:06 +0300

    Add CSV Handler Reader Class

---
+++ Marvel-Game/src/main/java/org/example/utils/CSVHandler.java
@@ -0,0 +1,33 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class CSVHandler {
+  public static List<String[]> load(String filename) {
+    BufferedReader br;
+    List<String[]> dataTable = new ArrayList<>();
+    try {
+      br = new BufferedReader(new FileReader(filename));
+      while (true) {
+        String line = br.readLine();
+        if (line == null)
+          break;
+        String[] row = line.split(",");
+        for (int j = 0; j < row.length; j++)
+          row[j] = row[j].trim();
+        dataTable.add(row);
+      }
+      br.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return dataTable;
+  }
+}
