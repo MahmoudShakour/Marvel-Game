3de5cd58cc38a63834609c0bb9b6e36a549a21c7
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-09-13 08:54:11 +0300

    Complete apply and remove effects logic functions

--- Marvel-Game/src/main/java/org/example/model/effects/Shield.java
+++ Marvel-Game/src/main/java/org/example/model/effects/Shield.java
@@ -1,9 +1,25 @@
 package model.effects;
 
+import model.world.Champion;
+
 public class Shield extends Effect {
+  private final static int SPEED_PERCENT = 102;
 
   public Shield(String name, int duration) {
     super(name, duration, EffectType.BUFF);
   }
 
+  @Override
+  public void apply(Champion champion) {
+    // Block the next attack or damaging ability cast on target.
+    int champtionNewSpeed = (int) (champion.getSpeed() * (SPEED_PERCENT / 100.0));
+    
+    champion.setSpeed(champtionNewSpeed);
+  }
+  
+  @Override
+  public void remove(Champion champion) {
+    int champtionNewSpeed = (int) (champion.getSpeed() * (100.0/SPEED_PERCENT));
+    champion.setSpeed(champtionNewSpeed);
+  }
 }
--- Marvel-Game/src/main/java/org/example/model/effects/Silence.java
+++ Marvel-Game/src/main/java/org/example/model/effects/Silence.java
@@ -1,9 +1,30 @@
 package model.effects;
 
+import model.world.Champion;
+
 public class Silence extends Effect {
 
   public Silence(String name, int duration) {
     super(name, duration, EffectType.DEBUFF);
+  }
+
+  @Override
+  public void apply(Champion champion) {
+    // Target cannot use abilities.
+    int championNewCurrentActionPoints = champion.getCurrentActionPoints() +2;
+    champion.setCurrentActionPoints(championNewCurrentActionPoints);
+    
+    int championNewMaxActionPointsPerTurn = champion.getMaxActionPointsPerTurn() + 2;
+    champion.setMaxActionPointsPerTurn(championNewMaxActionPointsPerTurn);
+  }
+
+  @Override
+  public void remove(Champion champion) {
+    int championNewCurrentActionPoints = champion.getCurrentActionPoints() -2;
+    champion.setCurrentActionPoints(championNewCurrentActionPoints);
+    
+    int championNewMaxActionPointsPerTurn = champion.getMaxActionPointsPerTurn() - 2;
+    champion.setMaxActionPointsPerTurn(championNewMaxActionPointsPerTurn);
   }
 
 }
