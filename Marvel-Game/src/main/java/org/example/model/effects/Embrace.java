8d0f014c6e8e20231e47b2db65136ab9424cc109
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-09-13 20:39:51 +0300

    Add Embrace Effect Behavior

--- Marvel-Game/src/main/java/org/example/model/effects/Embrace.java
+++ Marvel-Game/src/main/java/org/example/model/effects/Embrace.java
@@ -1,9 +1,28 @@
 package model.effects;
 
+import model.world.Champion;
+
 public class Embrace extends Effect {
+  private int INCREASE_PERCENTAGE = 120;
 
   public Embrace(String name, int duration) {
     super(name, duration, EffectType.BUFF);
+  }
+
+  @Override
+  public void apply(Champion champion) {
+    double ratio = (INCREASE_PERCENTAGE / 100.0);
+    champion.setCurrentHP((int) (champion.getMaxHP() * ratio));
+    champion.setSpeed((int) (champion.getSpeed() * ratio));
+    champion.setAttackDamage((int) (champion.getAttackDamage() * ratio));
+  }
+
+  @Override
+  public void remove(Champion champion) {
+    double ratio = (INCREASE_PERCENTAGE / 100.0);
+
+    champion.setSpeed((int) (champion.getSpeed() * (1.0 / ratio)));
+    champion.setAttackDamage((int) (champion.getAttackDamage() * (1.0 / ratio)));
   }
 
 }
