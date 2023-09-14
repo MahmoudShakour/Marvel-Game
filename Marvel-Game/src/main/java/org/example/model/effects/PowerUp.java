f5ec0e885f9ef9d29e72bc5ec7555bc5e77f8214
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-09-14 19:54:06 +0300

    Add PowerUp Effect Behavior

--- Marvel-Game/src/main/java/org/example/model/effects/PowerUp.java
+++ Marvel-Game/src/main/java/org/example/model/effects/PowerUp.java
@@ -1,9 +1,44 @@
 package model.effects;
 
+import model.abilities.Ability;
+import model.abilities.CrowdControlAbility;
+import model.abilities.DamagingAbility;
+import model.abilities.HealingAbility;
+import model.world.Champion;
+
 public class PowerUp extends Effect {
+  private double ratio = 120.0 / 100;
 
   public PowerUp(String name, int duration) {
     super(name, duration, EffectType.BUFF);
+  }
+
+  @Override
+  public void apply(Champion champion) {
+
+    for (Ability ability : champion.getAbilities()) {
+      if (ability instanceof HealingAbility) {
+        ((HealingAbility) ability).setHealAmount((int) (((HealingAbility) ability).getHealAmount() * ratio));
+      } else if (ability instanceof DamagingAbility) {
+        ((DamagingAbility) ability).setDamageAmount((int) (((DamagingAbility) ability).getDamageAmount() * ratio));
+
+      }
+    }
+  }
+
+  @Override
+  public void remove(Champion champion) {
+
+    for (Ability ability : champion.getAbilities()) {
+      if (ability instanceof HealingAbility) {
+        ((HealingAbility) ability)
+            .setHealAmount((int) (((HealingAbility) ability).getHealAmount() * 1.0 / ratio));
+      } else if (ability instanceof DamagingAbility) {
+        ((DamagingAbility) ability)
+            .setDamageAmount((int) (((DamagingAbility) ability).getDamageAmount() * 1.0 / ratio));
+
+      }
+    }
   }
 
 }
