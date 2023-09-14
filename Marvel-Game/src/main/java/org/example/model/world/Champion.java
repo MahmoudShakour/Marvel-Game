378452cdd43ab17bbe19c124e6cfa207c58308a6
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-09-14 20:34:27 +0300

    Complete Champion useLeaderAbility method

--- Marvel-Game/src/main/java/org/example/model/world/Champion.java
+++ Marvel-Game/src/main/java/org/example/model/world/Champion.java
@@ -2,11 +2,13 @@ package model.world;
 
 import model.abilities.Ability;
 import model.effects.Effect;
+import model.effects.Embrace;
+import model.effects.Stun;
 
 import java.awt.*;
 import java.util.ArrayList;
 
-public abstract class Champion implements Damageable {
+public abstract class Champion implements Damageable, Comparable<Champion> {
     private String name;
     private int maxHP;
     private int currentHP;
@@ -183,4 +185,52 @@ public abstract class Champion implements Damageable {
         this.dodgeChance = dodgeChance;
     }
 
+    @Override
+    public int compareTo(Champion champion) {
+        if (this.speed != champion.speed)
+            return champion.speed - this.speed;
+        else
+            return this.name.compareTo(champion.name);
+
+    }
+
+    private double getDistanceFrom(Champion target) {
+        double deltaX = this.location.x - target.location.x;
+        double deltaY = this.location.y - target.location.y;
+
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+
+    public void useLeaderAbility(ArrayList<Champion> targets) {
+        if (this instanceof Hero) {
+
+            // Todo:
+            // Removes all negative effects from the player’s entire team and adds an
+            // Embrace effect to them which lasts for 2 turns.
+
+            for (Champion champion : targets)
+                new Embrace("Embrace", 2).apply(champion);
+
+        } else if (this instanceof Villain) {
+
+            // Todo:
+            // Immediately eliminates (knocks out) all enemy champions with less than 30%
+            // health points
+
+            double ratio = 30.0 / 100;
+            for (Champion champion : targets)
+                if (champion.getCurrentHP() < Math.floor(champion.getCurrentHP() * ratio))
+                    champion.setCurrentHP(0);
+
+        } else if (this instanceof AntiHero) {
+
+            // Todo:
+            // All champions on the board except for the leaders of each team will be
+            // stunned for 2 turns.
+
+            for (Champion champion : targets)
+                new Stun("Stun", 2).apply(champion);
+
+        }
+    }
 }
